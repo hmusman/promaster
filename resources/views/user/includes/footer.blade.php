7669d7680de5b7d858a3db0e375903722499ad93
omarhussain-git <58678668+omarhussain-git@users.noreply.github.com>
2020-01-28 12:27:06 +0500

    remove language translator

--- resources/views/user/includes/footer.blade.php
+++ resources/views/user/includes/footer.blade.php
@@ -54,14 +54,14 @@ body {
     left: 25px;
 }
 			</style>
-			<div id="google_translate_element"></div>
+<!-- 			<div id="google_translate_element"></div>
 			<script type="text/javascript">
 function googleTranslateElementInit() {
     new google.translate.TranslateElement({
         pageLanguage: 'en'
     }, 'google_translate_element');
 }
-			</script>
+			</script> -->
 			<script type="text/javascript"
 			    src="http://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 			@stack('script');
