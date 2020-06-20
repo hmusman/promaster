82ee2a4c9733650c2a9726a29df9d43a54bdd93f
A4RehmanSE <fat32aa@gmail.com>
2020-06-20 23:19:35 +0500

    image fix.

--- resources/views/user/pages/certificateEmail-template.blade.php
+++ resources/views/user/pages/certificateEmail-template.blade.php
@@ -7,7 +7,7 @@
     <!-- <h2>Promaster Contact Message</h2> -->
     <h4>{{$details['subject']}}</h4>
     <div class="row">
-        <img src="{{url('public/assets/img/seo/ilus10.png')}}/{{$details['image']}}" alt="subject_thubmail" style="height: 200px; margin-left:2%;">
+        <img src="{{url('public/course-thumbnails')}}/{{$details['image']}}" alt="subject_thubmail" style="height: 200px; margin-left:2%;">
 
       <div class="col-md-6">
         <p style="margin-top: -21%; margin-left: 39%; word-break: normal;">{{$details['body']}}</p>
